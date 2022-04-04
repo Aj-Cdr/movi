0272b36f2edd7639d561a1f634917043ba2681e8
SPABOI <95515701+SPABOI@users.noreply.github.com>
2022-04-03 20:45:47 -0700

    Add files via upload

---
+++ imdb.js
@@ -0,0 +1,3 @@
+//imdb - refresh navigation website :D
+document.getElementById("movie-button").addEventListener("click", () => { location.href = "index.html" })
+document.getElementById("tv-shows-button").addEventListener("click", () => { location.href = "imdb-tv-shows.html" })
